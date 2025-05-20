be4bfc2e9e09975cbe3c9e34ea9f1f128d67cd8e
Albert-Peralta <20243ds068@utez.edu.mx>
2025-05-20 09:36:19 -0600

    Primer avance

---
+++ Practica1/src/App.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+         String Dato1 = JOptionPane.showInputDialog("Ingresa el primer número:");
+         String Dato2 = JOptionPane.showInputDialog("Ingresa el segundo número:");
+         String Dato3 = JOptionPane.showInputDialog("Ingresa el tercer número:");
+
+        int num1 = Integer.parseInt(Dato1);
+        int num2 = Integer.parseInt(Dato2);
+        int num3 = Integer.parseInt(Dato3);
+
+        
+
+         
+        
+
+    }
+}
